f87ac4e43a012b81d81181d29a7488c87790f93e
JuanCarrasquilla360 <juancarrasquilla135219@hotmail.com>
2023-03-01 19:05:48 -0500

    bg color list

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -8,10 +8,11 @@ export const List = ({ publisher }: { publisher: string }) => {
     const res = useSelector((state: any) => state.shows);
 
     return (
-        <div className="row rows-cols-1 row-cols-md-3 g-3 animate__animated animate__fadeIn">
+        <div className="row row-cols-md-3 row-cols-sm-1 g-3 animate__animated animate__fadeIn"
+            style={{ backgroundColor: "#eee" }}>
             {
                 res[publisher]?.map((show: any) => {
-                    const show1 = {...show, publisher}
+                    const show1 = { ...show, publisher }
                     return (
                         <Card
                             key={show.id}
